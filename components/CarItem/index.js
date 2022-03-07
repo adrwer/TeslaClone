12d7034e765bb8ebdb40939839e80a698a935802
Adrian Werimo <adrwer7@gmail.com>
2022-03-08 00:37:15 +0300

    Add styled button component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,7 +1,8 @@
 import { View, Text, ImageBackground } from 'react-native';
 import React from 'react';
 
-import styles  from './style';
+import styles from './styles';
+import StyledButton from '../StyledButton';
 
 const CarItem = () => {
   return (
@@ -16,6 +17,8 @@ const CarItem = () => {
           <Text style={styles.subtitle}>Starting at $69,420</Text>
         </View>
 
+        <StyledButton type='primary' content={'custom order'} onPress={() => {console.warn("custom order was pressed");}}/>
+        <StyledButton type='secondary' content={'existing inventory'} onPress={() => {console.warn("existing inventory was pressed");}}/>
     </View>
   );
 };
